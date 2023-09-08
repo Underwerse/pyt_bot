95c4e79adf69a4381cfe773effd6d7e9dd7d2816
Pavel Chernov <pvchernoff@gmail.com>
2023-09-08 20:16:17 +0300

    feat: add translator

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import os
 # Библиотеки для API-запросов
 import requests
+# Библиотека для перевода введенного юзером текста на англ.
+from googletrans import Translator
 
 # Загружаем переменные окружения из .env
 load_dotenv()
@@ -13,61 +15,107 @@ FOOD_API_KEY=os.getenv("FOOD_API_KEY")
 
 # Создаем экземпляр бота
 bot = telebot.TeleBot(TOKEN)
+# Создаем экземпляр переводчика
+global translator
+translator = Translator()
+
+# Определяем глобальные переменные для последующего обращения к API
+global instructionsRequired
+instructionsRequired = True
+# Глобальный словарь с переводами кухонь
+cuisine_translations = {
+    "Африканская": "African",
+    "Азиатская": "Asian",
+    "Американская": "American",
+    "Британская": "British",
+    "Кажунская": "Cajun",
+    "Карибская": "Caribbean",
+    "Китайская": "Chinese",
+    "Восточноевропейская": "Eastern European",
+    "Европейская": "European",
+    "Французская": "French",
+    "Немецкая": "German",
+    "Греческая": "Greek",
+    "Индийская": "Indian",
+    "Ирландская": "Irish",
+    "Итальянская": "Italian",
+    "Японская": "Japanese",
+    "Еврейская": "Jewish",
+    "Корейская": "Korean",
+    "Латиноамериканская": "Latin American",
+    "Средиземноморская": "Mediterranean",
+    "Мексиканская": "Mexican",
+    "Ближневосточная": "Middle Eastern",
+    "Скандинавская": "Nordic",
+    "Южная": "Southern",
+    "Испанская": "Spanish",
+    "Тайская": "Thai",
+    "Вьетнамская": "Vietnamese"
+}
+diet_translations = {
+    "Безглютеновая": "Gluten Free", 
+    "Вегетарианская": "Vegetarian", 
+    "Веганская": "Vegan", 
+    "Обычная": "Regular"
+}
+hide_markup = types.ReplyKeyboardRemove()
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     global chat_id
     chat_id = message.chat.id
-    markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
-    item1 = telebot.types.KeyboardButton("Выбери тип кухни")
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    item1 = types.KeyboardButton("Выбери тип кухни")
     markup.add(item1)
     bot.send_message(chat_id, "Хэлоу, я бот-объебот (в смысле объедение-бот), помогаю в поиске рецептов блюд", reply_markup=markup)
 
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    global selected_cuisine
-    global selected_diet
-    global cuisines
-    global diets
-    global ingredients
+    # Число кнопок меню в ряд
     row_width=3
+    global selected_cuisine, selected_diet, ingredients, translated_cuisines, translated_diets
+
     if message.text == "Выбери тип кухни":
-        cuisine_markup = telebot.types.ReplyKeyboardMarkup(row_width=row_width)
-        cuisines = ["African", "Asian", "American", "British", "Cajun", "Caribbean", "Chinese", "Eastern European",
-                    "European", "French", "German", "Greek", "Indian", "Irish", "Italian", "Japanese", "Jewish",
-                    "Korean", "Latin American", "Mediterranean", "Mexican", "Middle Eastern", "Nordic", "Southern",
-                    "Spanish", "Thai", "Vietnamese"]
+        cuisine_markup = types.ReplyKeyboardMarkup(row_width=row_width)
+        # Используем переведенные названия для отображения на кнопках меню
+        translated_cuisines = list(cuisine_translations.keys())
+        
         # Разбиваем массив на подмассивы по row_width элементов и добавляем их в клавиатуру
-        for i in range(0, len(cuisines), row_width):
-            row = cuisines[i:i+row_width]
+        for i in range(0, len(translated_cuisines), row_width):
+            row = translated_cuisines[i:i+row_width]
             cuisine_markup.row(*row)
         
         bot.send_message(chat_id, "Выбери тип кухни:", reply_markup=cuisine_markup)
-    elif message.text in cuisines:
+    elif message.text in translated_cuisines:
         selected_cuisine = message.text
         bot.send_message(chat_id, f"Ты выбрал тип кухни: {selected_cuisine}")
         
         # Создаем клавиатуру для выбора диеты
-        diet_markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
-        diets = ["Gluten Free", "Vegetarian", "Vegan", "Regular"]
+        diet_markup = types.ReplyKeyboardMarkup(row_width=2)
+        translated_diets = list(diet_translations.keys())
         
-        # Разбиваем массив на подмассивы по row_width элементов и добавляем их в клавиатуру
-        for i in range(0, len(diets), 2):
-            row = diets[i:i+2]
+        # Разбиваем массив на подмассивы по 2 элемента и добавляем их в клавиатуру
+        for i in range(0, len(translated_diets), 2):
+            row = translated_diets[i:i+2]
             diet_markup.row(*row)
         
         bot.send_message(chat_id, "Выбери диету:", reply_markup=diet_markup)
-    elif message.text in diets:
+    elif message.text in translated_diets:
         selected_diet = message.text
-        bot.send_message(chat_id, f"Ты выбрал диету: {selected_diet}\nТеперь перечисли через запятую, какие ты хочешь использовать ингредиенты")
+        bot.send_message(chat_id, f"Ты выбрал диету: {selected_diet}\nТеперь перечисли через запятую, какие ты хочешь использовать ингредиенты", reply_markup=hide_markup)
                 
         
     elif ', ' in message.text:
-        ingredients = [ingredient.strip() for ingredient in message.text.split(',')]
-        bot.send_message(chat_id, f"Ты выбрал ингредиенты: {', '.join(ingredients)}")
+        # ingredients = [ingredient.strip() for ingredient in message.text.split(',')]
+
+        # Переводим ингредиенты с русского на английский
+        # ingredients = [translator.translate(ingredient, src='ru', dest='en').text for ingredient in ingredients]
+        ingredients = translator.translate(message.text, src='ru', dest='en').text
+
+        bot.send_message(chat_id, f"Ты выбрал ингредиенты: {message.text}")
     else:
-        bot.send_message(chat_id, "Пожалуйста, используйте кнопки для выбора опции.")
+        bot.send_message(chat_id, "Пожалуйста, используй кнопки для выбора опции.")
 
 # Запуск бота
 if __name__ == "__main__":
